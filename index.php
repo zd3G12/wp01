c9ba4aa206fc1056981ce58d2a36411adde0251d
zd3G12 <zdterra@gmail.com>
2019-08-30 11:34:50 +0900

    タイトル変更

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <?php if (have_posts()): ?>
     <?php while (have_posts()): the_post(); ?>
       <article class="post">
-        <h2>あいうえお
+        <h2>かきくけこ
           <a href="<?php echo get_permalink(); ?>"><?php the_title();?></a>
         </h2>
         <p class="postmeta">
